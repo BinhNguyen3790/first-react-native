c91a1f26fadaf6d1050c6db8fc18e9b0f273ada1
llckumadevelop <kumauser01@kuma.ink>
2023-12-19 20:32:03 +0700

    add scrollview flatlist

--- App.tsx
+++ App.tsx
@@ -143,10 +143,12 @@
 import React from 'react';
 import HomeScreen from './screens/home/HomeScreen';
 import LoginScreen from './screens/login/LoginScreen';
+import DemoScroll from './screens/DemoScroll';
 const App = () => {
   return (
     // <HomeScreen/>
-    <LoginScreen />
+    // <LoginScreen />
+    <DemoScroll />
   )
 }
 export default App;
