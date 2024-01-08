95357667cbbf1e38f234a4ed12b0a15293822bb3
llckumadevelop <kumauser01@kuma.ink>
2024-01-08 21:33:10 +0700

    add header pages go back

--- App.tsx
+++ App.tsx
@@ -5,23 +5,34 @@ import DemoScroll from './screens/DemoScroll';
 import SectionListView from './screens/SectionListView';
 import ListCategory from './screens/home/ListCategory';
 import { NavigationContainer } from '@react-navigation/native';
-import { Button, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Home = ({navigation}:any) => {
+import Icon from 'react-native-vector-icons/AntDesign';
+
+const Home = ({ navigation }: any) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button title='change page' onPress={()=>navigation.navigate("Contact")}/>
+      <Button title='change page' onPress={() => navigation.navigate("Contact", { name: "binhDev", obj: { id: 1, price: 2999, name: "book" } })} />
     </View>
   )
 }
 
-const Contact = () => {
+const Contact = ({ route, navigation }: any) => {
+  const { name, obj } = route.params;
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Contact Screen</Text>
-    </View>
+    <>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text><Icon name="back" size={30} /> Go Back</Text>
+        </TouchableOpacity>
+        <Icon name="bells" size={30} />
+      </View>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Contact Screen nme: {name}</Text>
+      </View>
+    </>
   )
 }
 
@@ -37,12 +48,20 @@ const App = () => {
     // <SectionListView />
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home}></Stack.Screen>
-        <Stack.Screen name="Contact" component={Contact}></Stack.Screen>
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}></Stack.Screen>
+        <Stack.Screen name="Contact" component={Contact} options={{ headerShown: false }}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   )
 }
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    backgroundColor: 'grey'
+  }
+})
 export default App;
 
 
