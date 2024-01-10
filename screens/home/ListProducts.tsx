9520ba0632190c98b2be31e6a00d6ad7b724ce2d
llckumadevelop <kumauser01@kuma.ink>
2024-01-10 21:59:12 +0700

    update code

--- screens/home/ListProducts.tsx
+++ screens/home/ListProducts.tsx
@@ -1,41 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Category from '../components/Category'
 import { FlatList, Image, StyleSheet, Text, View } from "react-native";
 const ListProducts = () => {
-  const fakeData = [
-    {
-      id: 1, image: require("../../assets/imgs/pic_girl.jpg")
-    },
-    {
-      id: 2, image: require("../../assets/imgs/pic_girl.jpg")
-    },
-    {
-      id: 3, image: require("../../assets/imgs/pic_girl.jpg")
-    },
-    {
-      id: 4, image: require("../../assets/imgs/pic_girl.jpg")
-    },
-    {
-      id: 5, image: require("../../assets/imgs/pic_girl.jpg")
-    },
-    {
-      id: 6, image: require("../../assets/imgs/pic_girl.jpg")
-    },
-    {
-      id: 7, image: require("../../assets/imgs/pic_girl.jpg")
-    },
-    {
-      id: 8, image: require("../../assets/imgs/pic_girl.jpg")
-    },
-  ]
+  // const fakeData = [
+  //   {
+  //     id: 1, image: require("../../assets/imgs/pic_girl.jpg")
+  //   },
+  //   {
+  //     id: 2, image: require("../../assets/imgs/pic_girl.jpg")
+  //   },
+  //   {
+  //     id: 3, image: require("../../assets/imgs/pic_girl.jpg")
+  //   },
+  //   {
+  //     id: 4, image: require("../../assets/imgs/pic_girl.jpg")
+  //   },
+  //   {
+  //     id: 5, image: require("../../assets/imgs/pic_girl.jpg")
+  //   },
+  //   {
+  //     id: 6, image: require("../../assets/imgs/pic_girl.jpg")
+  //   },
+  //   {
+  //     id: 7, image: require("../../assets/imgs/pic_girl.jpg")
+  //   },
+  //   {
+  //     id: 8, image: require("../../assets/imgs/pic_girl.jpg")
+  //   },
+  // ]
+
+  const [games, setGames] = useState([]);
+
+  const getApi = () => {
+    return fetch("https://60b98fee80400f00177b68e5.mockapi.io/api/games")
+      .then((res) => res.json())
+      .then((data) => setGames(data))
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getApi();
+  }, [])
+
   return (
     <>
       <Category title="Danh Mục 2" />
-      <FlatList scrollEnabled={false} numColumns={2} columnWrapperStyle={styles.container} data={fakeData} renderItem={({ item }) =>
+      <FlatList scrollEnabled={false} numColumns={2} columnWrapperStyle={styles.container} data={games} renderItem={({ item }: any) =>
         <View style={styles.item}>
           <View style={styles.content}>
-            <Image style={styles.itemImg} source={item.image} />
-            <View style={styles.text}></View>
+            <Image style={styles.itemImg} source={{ uri: item.avatar }} />
+            <View style={styles.text}><Text style={styles.textTxt}>{item.name}</Text></View>
           </View>
         </View>
       } />
@@ -69,6 +83,12 @@ const styles = StyleSheet.create({
   text: {
     backgroundColor: "black",
     height: 40,
+    flexDirection:"row",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  textTxt: {
+    color: "white",
   },
   sale: {
     backgroundColor: "white",
